50217a6d8f0a312a49534a59805efa6df6f855ef
ARPITJAINROCK <31237598+ARPITJAINROCK@users.noreply.github.com>
2019-04-07 23:28:34 +0530

    Update ContentKNNAlgorithm.py

--- ContentKNNAlgorithm.py
+++ ContentKNNAlgorithm.py
@@ -1,4 +1,4 @@
-# This contains function that uses similarity matrix to compute recommendations
+# This contains function that uses similarity matrix to compute recommendations   
 from surprise import AlgoBase
 from surprise import PredictionImpossible
 from MovieLens import MovieLens
